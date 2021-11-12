cb9c51f731ec5a99ba24f2f6b148df6f7e70a91d
Kazım Etiksan <kazim@pikselmutfak.com>
2021-11-12 11:06:50 +0300

    updates

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-const {ClientSecretCredential} = require('@azure/identity')
-const {SecretClient} = require('@azure/keyvault-secrets')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const _ = require('lodash')
 
-let appInsights = require('applicationinsights')
-
-appInsights.setup()
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true, true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .setSendLiveMetrics(false)
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
-    .start()
-
 const app = express()
 app.use(bodyParser.json())
 
-const credential = new ClientSecretCredential(
-    '90386a23-5656-47c3-bd6f-ca12bf2b8ed4',  // tenantId
-    'afe7a68d-dc06-45d3-b408-802e57cd7b9b', // clientId
-    '97c7Q~bvFiupm45JCgIHnC5nbb.cFpmLVo1VW' // client secret
-)
-
-const client = new SecretClient('https://myvaultke.vault.azure.net/', credential)
-
-
 app.get('/api/hello', async (req, res) => {
 
-    const response = await client.getSecret('mysecret')
-    res.status(421).send('secret: '+response.value)
+    res.send('Hello World')
 })
 
 app.get('/api/merhaba', (req, res) => {
 
-    res.status(521).send('secret: '+response.value)
+    res.send('Merhaba Dünya')
 })
 
 app.post('/api/name', (req, res) => {
